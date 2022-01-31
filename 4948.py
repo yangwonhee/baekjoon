f7f2f75c6a60c72bded0210d85cb796862b504a2
yangwonhee <yangwonhee@kakao.com>
2022-01-31 19:07:28 +0900

    #4948 code

---
+++ 4948.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+max = 123456 * 2 + 1
+T = [True] * max
+# print(type(T))
+for i in range(2, int(sqrt(max)+1)):
+    if T[i]:
+        for j in range(2*i,max,i):
+            T[j] = False
+
+
+
+def countPrime(num):
+    cnt = 0
+    for i in range(num+1, num*2+1):
+        if T[i]:
+            cnt += 1
+    print(cnt)
+
+while True:
+    num = int(input())
+    if num == 0:
+        break
+    countPrime(num)
