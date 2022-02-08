a55bb003fdf95cf98b2fd3a3828b8b9b52292e27
yangwonhee <yangwonhee@kakao.com>
2022-02-08 15:50:23 +0900

    @1018 code

--- 1018.py
+++ 1018.py
@@ -1,6 +1,25 @@
 N, M = map(int, input().split())
-board = list()
-reset = list()
-for i in range(N):
-        board.append(input())
-cnt = 0
+origin = []
+count = []
+
+for _ in range(N):
+        origin.append(input())
+
+for a in range(N-7):
+        for b in range(M-7):
+                index1 = 0
+                index2 = 0
+                for i in range(a, a+8):
+                        for j in range(b, b+8):
+                                if (i+j) % 2 == 0:
+                                        if origin[i][j] != 'W':
+                                                index1 += 1
+                                        if origin[i][j] != 'B':
+                                                index2 += 1
+                                else:
+                                        if origin[i][j] != 'B':
+                                                index1 += 1
+                                        if origin[i][j] != 'W':
+                                                index2 += 1
+                count.append(min(index1, index2))
+print(min(count))
