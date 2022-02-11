cbe62c96458bad372ac4267b80a777dae5230a52
yangwonhee <yangwonhee@kakao.com>
2022-02-11 22:10:27 +0900

    # 10989 code

--- 10989.py
+++ 10989.py
@@ -1,10 +1,22 @@
-# 메모리 초과
+# # 메모리 초과
+
+# import sys
+# N = int(sys.stdin.readline())
+# data = [int(sys.stdin.readline()) for _ in range(N)]
+
+# data.sort()
+
+# for i in range(N):
+#     print(data[i])
 
 import sys
+
 N = int(sys.stdin.readline())
-data = [int(sys.stdin.readline()) for _ in range(N)]
+data = [0] * 10001
+for _ in range(N):
+    data[int(sys.stdin.readline())] += 1
 
-data.sort()
-
-for i in range(N):
-    print(data[i])
+for i in range(10001):
+    if data[i] != 0:
+        for j in range(data[i]):
+            print(i)
