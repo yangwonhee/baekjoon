8b540b7aa97fc0a241d2b7b5e14cb01a1c35d1a6
yangwonhee <yangwonhee@kakao.com>
2022-02-18 18:31:09 +0900

    #1541 code

---
+++ 1541.py
@@ -0,0 +1,13 @@
+import sys
+a = sys.stdin.readline().split('-')
+num = []
+for i in a:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
