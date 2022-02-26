7a3c65679f6600b7c642b778f9a4ca4b512b4540
yangwonhee <yangwonhee@kakao.com>
2022-02-26 16:22:37 +0900

    #5430 code

--- 5430.py
+++ 5430.py
@@ -8,20 +8,30 @@ for _ in range(t):
     p = str(sys.stdin.readline())
     n = int(sys.stdin.readline())
     arr = deque(sys.stdin.readline().rstrip()[1:-1].split(","))
+    deque(arr)
 
     if n == 0:
-        arr = deque()
+        arr = []
 
-    cnt = 0
+    check = 0
+    time = 0
     for i in p:
         if i == "R":
-            arr.reverse()
+            time += 1
         elif i == "D":
             if arr:
-                arr.popleft()
+                if time % 2 == 0:
+                    arr.popleft()
+                else:
+                    arr.pop()
             else:
                 print("error")
-                cnt = 1
+                check = 1
                 break
-    if cnt == 0:
-        print("["+",".join(arr)+"]")
+
+    if check == 0:
+        if time % 2 == 0:
+            print("["+",".join(arr)+"]")
+        else:
+            arr.reverse()
+            print("["+",".join(arr)+"]")
