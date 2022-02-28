795c1fb921e0bd0a7185d6d24f71e16dc0ec8c42
yangwonhee <yangwonhee@kakao.com>
2022-02-28 21:36:51 +0900

    #1012 code

--- 1012_x.py
+++ 1012_x.py
@@ -1,34 +1,33 @@
+from collections import deque
 t = int(input())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs(m, n, k):
-    bchu = [list(map(int, input().split())) for _ in range(k)]
-    graph = [[0 for _ in range(m+1)] for _ in range(n+1)]
-    # for i in range(n):
-    #     for j in range(m):
-    #         if bchu.count([i,j]) == 1:
-    #             graph[i][j] = 1
-    for i in bchu:
-        x, y = i
-        for j in range(4):
-            nx = x + dx[j]
-            ny = y + dy[j]
-            if 0 <= nx < m and 0 <= ny < n and bchu.count([nx, ny]) == 1:
+def bfs(x, y):
+    queue = deque([])
+    queue.append([x, y])
+    while queue:
+        a, b = queue.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
                 graph[nx][ny] = 0
+                queue.append([nx, ny])
 
-
-queue = []
 for i in range(t):
     m, n, k = map(int, input().split())
-    queue.append(dfs(m, n, k))
-    result = 0
-    for j in range(len(queue)):
-        if queue[j] == True:
-            result += 1
-    print(result)
+    graph = [[0 for _ in range(m)] for _ in range(n)]
+    count = 0
+    for j in range(k):
+        a, b = map(int, input().split())
+        graph[b][a] = 1
+    for l in range(n):
+        for p in range(m):
+            if graph[l][p] == 1:
+                bfs(l, p)
+                graph[l][p] = 0
+                count += 1
 
-# graph = [[0 for _ in range(m)] for _ in range(n)]
-# for i in bchu:
-#     [a, b] = i
+    print(count)
