cb1a5b28177df60c6f4d0810741ff272db0d263c
yangwonhee <yangwonhee@kakao.com>
2022-03-02 18:44:32 +0900

    #2206 code

--- 2206.py
+++ 2206.py
@@ -1,34 +1,35 @@
 from collections import deque
-n, m = map(int, input().split())
-graph = []
-for i in range(n):
-        graph.append(list(map(int,input())))
-# print(graph)
+import sys
+input = sys.stdin.readline
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-queue = deque([])
 
-def bfs(a, b, t):
-    queue.append([a, b, t])
+def bfs():
+    queue = deque()
+    queue.append([0, 0, 1])
+    check = [[[0] *2 for _ in range(m)]for _ in range(n)]
+    check[0][0][1] = 1
     while queue:
-        x, y, time = queue.popleft()
+        x, y, k = queue.popleft()
+        if x == n-1 and y == m-1:
+            return check[x][y][k]
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 0:
-                    graph[nx][ny] = graph[x][y] + 1
-                    queue.append([nx, ny, time])
-                elif graph[nx][ny] == 1:
-                    if time == 0:
-                        time += 1
-                        graph[nx][ny] = graph[x][y] + 1
-                        queue.append([nx,ny, time])
-                    else:
-                        continue 
-    if graph[n-1][m-1] != 0:
-        return graph[n-1][m-1]
-    else:
-        return -1
+                if graph[nx][ny] == 1 and k == 1:
+                        check[nx][ny][0] = check[x][y][1] + 1
+                        queue.append([nx, ny, 0])
+                elif graph[nx][ny] == 0 and check[nx][ny][k] == 0:
+                    check[nx][ny][k] = check[x][y][k] + 1
+                    queue.append([nx, ny, k])
+    return -1
 
-print(bfs(0, 0, 0))
+
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int,list(input().strip()))))
+
+print(bfs())
