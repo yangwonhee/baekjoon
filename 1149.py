11752c66c49f0a3f423cdbd0f3e8cafb9be386de
yangwonhee <yangwonhee@kakao.com>
2022-03-03 21:51:37 +0900

    #1149 code

---
+++ 1149.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+house = [[] for _ in range(n)]
+for i in range(n):
+    house[i] = list(map(int, input().split()))
+
+for i in range(1, n):
+    house[i][0] = min(house[i-1][1], house[i-1][2]) + house[i][0]
+    house[i][1] = min(house[i-1][0], house[i-1][2]) + house[i][1]
+    house[i][2] = min(house[i-1][0], house[i-1][1]) + house[i][2]
+print(min(house[n-1]))
